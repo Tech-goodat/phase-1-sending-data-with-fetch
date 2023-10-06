24bb885b3434aff900aedce122acd4a94667feff
Tech-goodat <felixkiprotich2000@gmail.com>
2023-10-06 12:55:02 +0300

    done with assignmet

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+const formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle",
+  };
+ const configurationObject ={
+    method: "POST",
+    headers:{
+        "content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({
+        dogName:"Byron",
+        dogBreed:"Poodie",
+    })
+}
+
+fetch("http://localhost:3000/dogs", configurationObject)
+.then(function(response){
+    return response.json();
+})
+.then(function (object){
+    console.log(object);
+});
+
+
